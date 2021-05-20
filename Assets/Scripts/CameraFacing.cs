18267fadc5dcb5ab740fc0807e39b74e88f5bb96
Saxdel <84539802+HincuAndrei@users.noreply.github.com>
2021-05-21 01:41:19 +0300

    HUD
    Health and mana bar

---
+++ Assets/Scripts/CameraFacing.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.CORE
+{
+    public class CameraFacing : MonoBehaviour
+    {
+         void Update()
+        {
+            transform.forward = Camera.main.transform.forward;
+        }
+    }
+}
